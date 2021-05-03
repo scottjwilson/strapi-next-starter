22558758b2788677cc089f281afb27635ae69a76
Scott <slimzel@protonmail.com>
2021-05-03 16:07:05 -0700

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8abc7832050e935ca9d872c04de54527'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ee7da5e-8bc0-44e7-9dc7-b093247bcd92'
+};
